75e105f751bc62e0e3a98ada7e0184d91948b711
Alkesst <alec.augal@gmail.com>
2018-11-21 17:49:13 +0100

    Improved the login form and conceptual model

--- src/ACOESApp.java
+++ src/ACOESApp.java
@@ -1,2 +1,9 @@
+import javax.swing.*;
+
 public class ACOESApp {
+    private JButton loginButton;
+    private JPasswordField passwordField;
+    private JTextField emailField;
+    private JLabel appName;
+    private JPanel loginWindow;
 }
