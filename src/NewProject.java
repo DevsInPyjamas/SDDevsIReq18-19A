fa46c688f2df533558d6ea698428ff758dc32bfc
Wan Garcia <jugr98@gmail.com>
2018-11-24 12:46:47 +0100

    Names changed in NewProject

--- src/NewProject.java
+++ src/NewProject.java
@@ -1,4 +1,21 @@
-import java.swing.JPanel;
+import java.swing.*;
 
 public class NewProject extends JPanel{
+    private JButton backButton;
+    private JLabel nuevoProyectoLabel;
+    private JLabel nombreLabel;
+    private JTextField nombreField;
+    private JLabel tipoProyectoLabel;
+    private JComboBox tipoProyectoComboBox;
+    private JLabel ubicacionLabel;
+    private JTextField ubicacionField;
+    private JLabel coordinadorLabel;
+    private JTextField coordinadorLabel;
+    private JLabel economicoLabel;
+    private JTextField economicoField;
+    private JLabel poblacionLabel;
+    private JTextField poblacionField;
+    private JLabel departamentoLabel;
+    private JTextField departamentoField;
+    private JButton anadirButton;
 }
