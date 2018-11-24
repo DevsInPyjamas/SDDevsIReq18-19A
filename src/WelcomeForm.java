39b2a5757accc982ed34bafeaadb8e039d022c12
Wan Garcia <jugr98@gmail.com>
2018-11-24 13:33:10 +0100

    Names changed in WelcomeForm

--- src/WelcomeForm.java
+++ src/WelcomeForm.java
@@ -1,9 +1,9 @@
 import javax.swing.*;
 
 public class WelcomeForm {
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
+    private JButton cerrarSesionButton;
+    private JButton economicoButton;
+    private JButton becasButton;
+    private JButton adminButton;
     private JLabel bienvenidoLabel;
 }
