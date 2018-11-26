94aa255a9e707d110bd5c21fda6c0be43f27a934
Wan Garcia <jugr98@gmail.com>
2018-11-26 19:02:08 +0100

    SearchUser interface added

--- src/SearchUser.java
+++ src/SearchUser.java
@@ -1,2 +1,10 @@
+import javax.swing.*;
+
 public class SearchUser {
+    private JPanel buscarUserPanel;
+    private JButton backButton;
+    private JLabel busquedaUsuarioLabel;
+    private JTextField buscadorField;
+    private JButton buscadorButton;
+    private JTable usuariosTable;
 }
