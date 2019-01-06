7b09cc48f74b89dbe1d01a631a322258ec9739ec
Alkesst <alec.augal@gmail.com>
2019-01-06 20:15:02 +0100

    Cosas

--- src/NewEconomicMovement.java
+++ src/NewEconomicMovement.java
@@ -1,7 +1,4 @@
-import db_management.DBManager;
-import db_management.TipoGasto;
-import db_management.Usuario;
-import db_management.Transaccion;
+import db_management.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -15,6 +12,8 @@ public class NewEconomicMovement {
     private JTextField conceptoField;
     private JTextField cantidadField;
     private JComboBox tipoGastoComboBox;
+    private JComboBox empresaComboBox;
+    private JComboBox socioComboBox;
     private Usuario loggedUser;
 
     NewEconomicMovement(Usuario loggedUser) {
@@ -32,6 +31,17 @@ public class NewEconomicMovement {
         for(Object[] tuple : queryTuples) {
             tipoGastoComboBox.addItem(tuple[0]);
         }
+        queryTuples = dbManager.select("select nombre from Empresa;");
+        for(Object[] tuple : queryTuples) {
+            empresaComboBox.addItem(tuple[0]);
+        }
+        queryTuples = dbManager.select("select isDeleted, concat(nombre, ' ', apellidos)" + " from Socio " +
+                "where asociation = '" + loggedUser.getProyecto().getId() + "';");
+        for(Object[] tuple : queryTuples) {
+            if(!(boolean) tuple[0]) {
+                socioComboBox.addItem(tuple[1]);
+            }
+        }
         backButton.addActionListener((e) -> {
             if (e.getActionCommand().equals("Atrás")) {
                 new EconomicSection(loggedUser);
@@ -39,7 +49,7 @@ public class NewEconomicMovement {
             }
         });
         addEconomicButton.addActionListener((e) -> {
-            if (checkIfThereAreNotBlankFields()) {
+            if (everyMandatoryFieldIsFilled()) {
                 JOptionPane.showMessageDialog(new JFrame(), "Hay campos obligatorios en blanco");
             } else {
                 Transaccion t = new Transaccion();
@@ -50,11 +60,21 @@ public class NewEconomicMovement {
                 t.setTipoGasto(new TipoGasto(tipoGastoID));
                 t.setCantidad(Double.valueOf(cantidadField.getText()));
                 t.setProyecto(loggedUser.getProyecto());
-                t.save();
-                /*
-                emisorField.getText(), conceptoField.getText(), cantidadField.getText(),
-                        Objects.requireNonNull(tipoGastoComboBox.getSelectedItem()).toString()
-                 */
+                if(socioComboBox.getSelectedItem() != null) {
+                    int socioID = (int) dbManager.select("select id from Socio where " +
+                            "contat(nombre, ' ', apellidos) = '" + socioComboBox.getSelectedItem() + "';").get(0)[0];
+                    t.setSocio(new Socio(socioID));
+                }
+                if(empresaComboBox.getSelectedItem() != null) {
+                    int empresaID = (int) dbManager.select("select id from Empresa where nombre like '" + 
+                            empresaComboBox.getSelectedItem() + "';").get(0)[0];
+                    t.setEmpresa(new Empresa(empresaID));
+                }
+                try {
+                    t.save();
+                } catch (Exception e1) {
+                    JOptionPane.showMessageDialog(new JFrame(), "Los datos introducidos no son correctos");
+                }
                 JOptionPane.showMessageDialog(new JFrame(), "Los datos introducidos son correctos");
                 new EconomicSection(loggedUser);
                 frame.dispose();
@@ -62,7 +82,9 @@ public class NewEconomicMovement {
         });
     }
 
-    private boolean checkIfThereAreNotBlankFields() {
-        return false;
+    private boolean everyMandatoryFieldIsFilled() {
+        return (empresaComboBox.getSelectedItem() != null || socioComboBox.getSelectedItem() != null) 
+                && tipoGastoComboBox.getSelectedItem() != null && !emisorField.getText().isEmpty() &&
+                !cantidadField.getText().isEmpty();
     }
 }
