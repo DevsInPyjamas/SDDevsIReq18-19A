bbe8e4b5b5fd0104344d80233b36669f9703dcd5
Alkesst <alec.augal@gmail.com>
2019-01-10 20:35:34 +0100

    Finished some interfaces bro

--- src/EconomicBalanceSection.java
+++ src/EconomicBalanceSection.java
@@ -1,9 +1,31 @@
 import db_management.Usuario;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
 public class EconomicBalanceSection {
     private Usuario loggedUser;
+    private JButton atrasButton;
+    private JLabel balanceLabel;
+    private JTextField beginningDateTextField;
+    private JTextField finishDateTextField;
+    private JButton searchButton;
+    private JTable balanceTable;
+    private JPanel balanceSectionPanel;
 
     public EconomicBalanceSection(Usuario loggedUser) {
-        this.loggedUser = loggedUser;
+        this.loggedUser= loggedUser;
+        balanceSectionPanel.setSize(700, 500);
+        JFrame frame= new JFrame("Buscar Movimentos Economicos");
+        frame.setBounds(400,400,300,200);
+        frame.setMinimumSize(new Dimension(700, 500));
+        frame.setContentPane(balanceSectionPanel);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        balanceTable.setModel(new DefaultTableModel(new Object[]{"id", "Emisor", "Cantidad", "Tipo de Gasto"}, 6));
+        DefaultTableModel model = (DefaultTableModel) balanceTable.getModel();
+        balanceTable.setFocusable(false);
+        balanceTable.setRowSelectionAllowed(false);
     }
 }
--- src/ValidarTransacciones.java
+++ src/ValidarTransacciones.java
@@ -1,9 +1,31 @@
 import db_management.Usuario;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
 public class ValidarTransacciones {
     private Usuario loggedUser;
+    private JButton atrasButton;
+    private JTable validateTransactionTable;
+    private JPanel validateTransactionPanel;
 
     public ValidarTransacciones(Usuario loggedUser) {
-        this.loggedUser = loggedUser;
+        this.loggedUser= loggedUser;
+        validateTransactionPanel.setSize(700, 500);
+        JFrame frame= new JFrame("Buscar Movimentos Economicos");
+        frame.setBounds(400,400,300,200);
+        frame.setMinimumSize(new Dimension(700, 250));
+        frame.setContentPane(validateTransactionPanel);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        validateTransactionTable.setModel(new DefaultTableModel(new Object[]{"id", "Emisor", "Cantidad", "Tipo de Gasto"}, 6));
+        DefaultTableModel model = (DefaultTableModel) validateTransactionTable.getModel();
+        validateTransactionTable.setFocusable(false);
+        validateTransactionTable.setRowSelectionAllowed(false);
+        atrasButton.addActionListener(e -> {
+            new EconomicSection(loggedUser);
+            frame.dispose();
+        });
     }
 }
