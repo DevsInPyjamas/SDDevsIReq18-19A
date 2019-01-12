2b952b0097ae8e4985dd85e232e58182a91fccad
Alkesst <alec.augal@gmail.com>
2019-01-12 18:20:15 +0100

    Balance listo

--- src/EconomicBalanceSection.java
+++ src/EconomicBalanceSection.java
@@ -1,8 +1,11 @@
+import db_management.DBManager;
 import db_management.Usuario;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.math.BigDecimal;
+import java.util.List;
 
 public class EconomicBalanceSection {
     private Usuario loggedUser;
@@ -13,6 +16,8 @@ public class EconomicBalanceSection {
     private JButton searchButton;
     private JTable balanceTable;
     private JPanel balanceSectionPanel;
+    private DBManager dbManager = new DBManager();
+    private double balance;
 
     public EconomicBalanceSection(Usuario loggedUser) {
         this.loggedUser= loggedUser;
@@ -23,9 +28,38 @@ public class EconomicBalanceSection {
         frame.setContentPane(balanceSectionPanel);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        balanceTable.setModel(new DefaultTableModel(new Object[]{"id", "Emisor", "Cantidad", "Tipo de Gasto"}, 6));
+        balanceTable.setModel(new DefaultTableModel(new Object[]{"id", "Beneficiario", "Cantidad", "Tipo de Gasto", "Fecha"}, 6));
         DefaultTableModel model = (DefaultTableModel) balanceTable.getModel();
         balanceTable.setFocusable(false);
         balanceTable.setRowSelectionAllowed(false);
+        this.balanceLabel.setText("0");
+        atrasButton.addActionListener(e -> {
+            new EconomicSection(loggedUser);
+            frame.dispose();
+        });
+        searchButton.addActionListener(e -> {
+            model.setRowCount(0);
+            String begginningDateValue = (beginningDateTextField.getText().isEmpty()) ? null  :
+                    "'" + beginningDateTextField.getText() + "'";
+            String finnishDateValue = (finishDateTextField.getText().isEmpty()) ? null :
+                    "'" + finishDateTextField.getText() + "'";
+            Integer valueOfID = (loggedUser.getProyecto() == null) ? null : loggedUser.getProyecto().getId();
+            List<Object[]> query = dbManager.select("exec TransaccionesRango " +  begginningDateValue + ", " +
+                    finnishDateValue + ", " + valueOfID);
+            for(Object[] tuple : query) {
+                if((boolean) tuple[6] && !(boolean) tuple[4]) {
+                    Object[] row = new Object[tuple.length];
+                    row[0] = tuple[0];
+                    BigDecimal a = (BigDecimal) tuple[3];
+                    balance += a.doubleValue();
+                    row[1] = dbManager.select("exec DatosBeneficiario " + tuple[0]).get(0)[1];
+                    row[2] = tuple[3];
+                    row[3] = dbManager.select("select nombre from TipoGasto where id = " + tuple[5]).get(0)[0];
+                    row[4] = tuple[9];
+                    model.addRow(row);
+                }
+            }
+            balanceLabel.setText(Double.toString(balance));
+        });
     }
 }
